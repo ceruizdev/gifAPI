3cd11b5b194be8d9fe48e45fa520f001ad9e1eba
Carlos Ruiz <ceruiz.dev@gmail.com>
2023-01-17 11:38:13 -0500

    http protocol changed to https

--- src/helpers/GetGIFs.js
+++ src/helpers/GetGIFs.js
@@ -1,6 +1,6 @@
 export const getGIFs = async (category) => {
     const APIKey = 'PhOn0GnCoWXNmuh21AW0HnuJwEstlP1O';
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${category}&limit=20`;
     const resp = await fetch(url);
     const { data } = await resp.json()
     const GIFs = data.map(img => ({
